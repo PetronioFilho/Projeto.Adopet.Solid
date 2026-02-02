8d3d2c94d109afdb870275b093fbf3088208084e
PetronioFilho <camposfilho84@gmail.com>
2026-02-02 16:37:25 -0300

    Implementação do Template Method

---
+++ src/Alura.Adopet.Console/Servicos/Arquivos/ClientesDoCsv.cs
@@ -0,0 +1,23 @@
+﻿using Alura.Adopet.Console.Modelos;
+
+namespace Alura.Adopet.Console.Servicos.Arquivos
+{
+    public class ClientesDoCsv : LeitorDeArquivosCsv<Cliente>
+    {
+        public ClientesDoCsv(string caminhoDoArquivoASerLido) : base(caminhoDoArquivoASerLido)
+        {
+        }
+
+        public override Cliente CriarDaLinhaCsv(string linha)
+        {
+            string[] propriedades = linha.Split(';');
+
+            return new Cliente
+                (
+                    id: Guid.Parse(propriedades[0]),
+                    nome: propriedades[1],
+                    email: propriedades[2]
+                );
+        }
+    }
+}
